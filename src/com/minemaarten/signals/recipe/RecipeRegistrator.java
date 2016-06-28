1b21d9bb510cde1ea00288a0df29a24590707f1f
MineMaarten <loopycoaster_8@hotmail.com>
2016-06-28 16:09:04 +0200

    Bugfix: Rail Network Controller does not have a recipe.

--- src/com/minemaarten/signals/recipe/RecipeRegistrator.java
+++ src/com/minemaarten/signals/recipe/RecipeRegistrator.java
@@ -5,8 +5,10 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 import com.minemaarten.signals.init.ModBlocks;
+import com.minemaarten.signals.init.ModItems;
 
 public class RecipeRegistrator {
 	public static void init(){
@@ -16,9 +18,15 @@ public class RecipeRegistrator {
 		register(new ItemStack(ModBlocks.pathSignal, 8), "sss", "srs", "sds", 's', "ingotGold", 'r', "dustRedstone", 'd', "dyeGreen");
 		register(new ItemStack(ModBlocks.pathSignal, 8), "sss", "srs", "sds", 's', "ingotGold", 'r', "dyeRed", 'd', "dyeGreen");
 		register(new ItemStack(ModBlocks.stationMarker), " i ", " g ", "iii", 'i', "ingotIron", 'g', "dyeGreen");
+		register(new ItemStack(ModItems.railNetworkController), "qgq", "bpb", "qdq", 'q', "gemQuartz", 'g', "dustGlowstone", 'b', Blocks.STONE_BUTTON, 'p', ModBlocks.pathSignal, 'd', "gemDiamond");
+		registerShapeless(new ItemStack(ModItems.cartEngine), Blocks.FURNACE, Blocks.GOLD_BLOCK);
 	}
 	
 	private static void register(ItemStack output, Object... input){
 		GameRegistry.addRecipe(new ShapedOreRecipe(output, input));
+	}
+	
+	private static void registerShapeless(ItemStack output, Object... input){
+		GameRegistry.addRecipe(new ShapelessOreRecipe(output, input));
 	}
 }
