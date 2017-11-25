0b2a92a6767fd5c12a011824b4eb429cacf877b9
MineMaarten <loopycoaster_8@hotmail.com>
2017-11-25 10:02:32 +0100

    Signals now only emit redstone to their left (to allow for more compact set-ups.

--- src/com/minemaarten/signals/block/BlockSignalBase.java
+++ src/com/minemaarten/signals/block/BlockSignalBase.java
@@ -85,7 +85,7 @@ public class BlockSignalBase extends BlockBase{
 
     @Override
     public int getWeakPower(IBlockState state, IBlockAccess worldIn, BlockPos pos, EnumFacing side){
-        if(!(worldIn instanceof WorldServer) || state.getBlock() != this || state.getValue(LAMP_STATUS) != EnumLampStatus.GREEN) return 0;
+        if(!(worldIn instanceof WorldServer) || state.getBlock() != this || state.getValue(LAMP_STATUS) != EnumLampStatus.GREEN || state.getValue(BlockSignalBase.FACING).rotateY() != side) return 0;
         TileEntitySignalBase signal = (TileEntitySignalBase)worldIn.getTileEntity(pos);
         signal.setWorld((WorldServer)worldIn);
         for(RailWrapper rail : signal.getConnectedRails()) {
