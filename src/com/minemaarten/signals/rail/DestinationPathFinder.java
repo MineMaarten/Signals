c03509f8b1d7943dd305107bb08e2007da9dd9db
MineMaarten <loopycoaster_8@hotmail.com>
2017-11-26 10:34:21 +0100

    Improved the pathfinding a bit in preparation of Chain Signal improvements. Fixes #13 too.

--- src/com/minemaarten/signals/rail/DestinationPathFinder.java
+++ src/com/minemaarten/signals/rail/DestinationPathFinder.java
@@ -3,7 +3,6 @@ package com.minemaarten.signals.rail;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -12,17 +11,17 @@ import java.util.regex.Pattern;
 
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
-
-import org.apache.commons.lang3.tuple.ImmutablePair;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.WorldServer;
 
 import com.minemaarten.signals.block.BlockSignalBase.EnumLampStatus;
-import com.minemaarten.signals.lib.Log;
 import com.minemaarten.signals.tileentity.TileEntitySignalBase;
 
 public class DestinationPathFinder{
+    private static final double RED_SIGNAL_PENALTY = 10000;
+
     public static class AStarRailNode implements Comparable<AStarRailNode>{
-        private int distanceFromStart = Integer.MAX_VALUE;
+        private int distanceFromGoal = Integer.MAX_VALUE;
         private AStarRailNode prevNode;
         private final RailWrapper goal;
         private final RailWrapper rail;
@@ -35,10 +34,10 @@ public class DestinationPathFinder{
         }
 
         public boolean checkImprovementAndUpdate(AStarRailNode node){
-            int nodeDist = node.distanceFromStart + 1;
-            if(nodeDist < distanceFromStart) {
+            int nodeDist = node.distanceFromGoal + 1;
+            if(nodeDist < distanceFromGoal) {
                 prevNode = node;
-                distanceFromStart = nodeDist;
+                distanceFromGoal = nodeDist;
                 return true;
             } else {
                 return false;
@@ -58,7 +57,7 @@ public class DestinationPathFinder{
         }
 
         private double getCost(){
-            return distanceFromStart + (goal != null ? getDistance(goal) : 0); //A*
+            return distanceFromGoal + (goal != null ? getDistance(goal) : 0); //A*
         }
 
         public EnumFacing getPathDir(){
@@ -95,48 +94,26 @@ public class DestinationPathFinder{
         Set<RailWrapper> traversedRails = new HashSet<>();
         Map<RailWrapper, AStarRailNode> railMap = new HashMap<>();
 
-        Map<RailWrapper, AStarRailNode> firstSignals = getFirstSignals(start, direction, goals);
-        Log.debug("Signals: " + firstSignals.size());
-        if(firstSignals.isEmpty()) return null; //When no destination directly found or signals found, break right away.
-        int minSignalDistance = getClosestSignal(firstSignals);
-
-        AStarRailNode bestRoute = null;
-        int bestDistance = Integer.MAX_VALUE;
+        AStarRailNode bestRoute = null; //In practice there will only be one route, as paths are created at a signal, which cannot be on an intersection.
 
         for(RailWrapper goal : goals) {
             AStarRailNode goalNode = new AStarRailNode(goal, null, start);
-            goalNode.distanceFromStart = 0;
+            goalNode.distanceFromGoal = 0;
             queue.add(goalNode);
             railMap.put(goal, goalNode);
         }
 
         while(!queue.isEmpty()) {
             AStarRailNode node = queue.remove(); //Take the node with the highest priority of the queue.
-            //  if(node.rail.world instanceof WorldServer) ((WorldServer)node.rail.world).spawnParticle(EnumParticleTypes.REDSTONE, node.rail.getX() + 0.5, node.rail.getY() + 0.5, node.rail.getZ() + 0.5, 1, 0, 0, 0, 0D);
+            if(node.rail.world instanceof WorldServer) ((WorldServer)node.rail.world).spawnParticle(EnumParticleTypes.REDSTONE, node.rail.getX() + 0.5, node.rail.getY() + 0.5, node.rail.getZ() + 0.5, 1, 0, 0, 0, 0D);
             //   else Log.debug("World: " + node.rail.world);
             traversedRails.add(node.rail); //Mark it as traversed so we won't put it on the queue anymore.
-            if(node.rail == start) continue;
 
-            AStarRailNode signalNode = firstSignals.get(node.rail);
-            if(signalNode != null) {
-                int signalWeight = signalNode.distanceFromStart;
-                firstSignals.remove(node.rail); //Only allow one path to end up on this signal.
-                int totalWeight = signalWeight + node.distanceFromStart;
-                if(totalWeight < bestDistance) {
-                    bestDistance = totalWeight;
-                    bestRoute = signalNode;
-                    Log.debug("New best: " + signalWeight);
-                }
-                if(firstSignals.isEmpty()) {
-                    Log.debug("No signals left: " + signalWeight);
-                    break;
-                }
-                minSignalDistance = getClosestSignal(firstSignals);
-                continue;
-            } else {
-                int minBound = minSignalDistance + node.distanceFromStart;
-                if(minBound > bestDistance) break; //Branch and bound, return when we can't ever find a better solution in best case.
+            //Branch and bound as soon as we get nodes that cannot get better than our current solution.
+            if(bestRoute != null && node.distanceFromGoal >= bestRoute.distanceFromGoal) {
+                break;
             }
+
             for(Map.Entry<RailWrapper, EnumFacing> entry : node.rail.getNeighborsForEntryDir(node.pathDir).entrySet()) {
                 RailWrapper neighborRail = entry.getKey();
                 if(TileEntitySignalBase.getNeighborSignal(neighborRail, entry.getValue().getOpposite()) == null && !traversedRails.contains(neighborRail)) {
@@ -146,7 +123,21 @@ public class DestinationPathFinder{
                         railMap.put(neighborRail, neighborNode);
                     }
                     if(neighborNode.checkImprovementAndUpdate(node)) {
+                        //Penalize red signals on the way
+                        TileEntitySignalBase signal = TileEntitySignalBase.getNeighborSignal(neighborNode.rail, neighborNode.pathDir);
+                        if(signal != null && signal.getLampStatus() == EnumLampStatus.RED) {
+                            neighborNode.distanceFromGoal += RED_SIGNAL_PENALTY;
+                        }
+
                         queue.add(neighborNode);
+
+                        if(neighborNode.rail == start) { //If we find the start, we have found a valid path from start to end.
+
+                            //If it's a better solution
+                            if(bestRoute == null || neighborNode.distanceFromGoal < bestRoute.distanceFromGoal) {
+                                bestRoute = neighborNode;
+                            }
+                        }
                     }
                 }
             }
@@ -163,62 +154,5 @@ public class DestinationPathFinder{
         }
 
         return bestRoute;
-    }
-
-    private static int getClosestSignal(Map<RailWrapper, AStarRailNode> signals){
-        int closest = Integer.MAX_VALUE;
-        for(AStarRailNode node : signals.values()) {
-            if(node.distanceFromStart < closest) {
-                closest = node.distanceFromStart;
-            }
-        }
-        return closest;
-    }
-
-    private static Map<RailWrapper, AStarRailNode> getFirstSignals(RailWrapper start, EnumFacing dir, Collection<RailWrapper> goals){
-        Map<RailWrapper, AStarRailNode> firstSignals = new HashMap<>();
-        Set<RailWrapper> traversedRails = new HashSet<>();
-        Queue<Map.Entry<RailWrapper, AStarRailNode>> traversingRails = new LinkedList<>();
-
-        for(Map.Entry<RailWrapper, EnumFacing> entry : start.getNeighborsForEntryDir(dir).entrySet()) {
-            if(entry.getValue() != dir.getOpposite()) {
-                AStarRailNode node = new AStarRailNode(entry.getKey(), entry.getValue(), null);
-                node.distanceFromStart = 0;
-                traversingRails.add(new ImmutablePair<>(entry.getKey(), node));
-            }
-        }
-        traversedRails.add(start); //Make sure to consider this block as traversed already, prevents traversing the tracks in reverse direction
-
-        while(!traversingRails.isEmpty()) {
-
-            Map.Entry<RailWrapper, AStarRailNode> neighbor = traversingRails.poll();
-            if(goals.contains(neighbor.getKey())) { //If we found the destination (without passing a signal)
-                firstSignals.clear();
-                firstSignals.put(neighbor.getKey(), neighbor.getValue());
-                Log.debug("Found destination without passing signals!");
-                return firstSignals;//Prefer that route
-            }
-
-            traversedRails.add(neighbor.getKey());
-            TileEntitySignalBase signal = TileEntitySignalBase.getNeighborSignal(neighbor.getKey(), neighbor.getValue().pathDir.getOpposite());
-            if(signal == null) { //If not connected to a signal
-                if(neighbor.getKey().getSignals().size() != 1) {//Try to find a single opposing signal instead.
-                    for(Map.Entry<RailWrapper, EnumFacing> entry : neighbor.getKey().getNeighborsForEntryDir(neighbor.getValue().pathDir).entrySet()) {
-                        BlockPos nextNeighbor = entry.getKey();
-                        if(!traversedRails.contains(nextNeighbor)) {
-                            AStarRailNode nextNode = new AStarRailNode(entry.getKey(), entry.getValue(), null);
-                            nextNode.checkImprovementAndUpdate(neighbor.getValue());
-
-                            traversingRails.add(new ImmutablePair<>(entry.getKey(), nextNode));
-                        }
-                    }
-                }
-            } else { //When connected to a signal
-                AStarRailNode node = neighbor.getValue();
-                firstSignals.put(neighbor.getKey(), node);
-                if(node != null && signal.getLampStatus() == EnumLampStatus.RED) node.distanceFromStart += 10000;
-            }
-        }
-        return firstSignals;
     }
 }
